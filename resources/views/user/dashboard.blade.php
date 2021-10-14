b1894f087eae0896117e9a095932408037a6da29
Shimada <my.sn.0308@gmail.com>
2021-10-14 13:32:08 +0900

    user.dashboard.bladeで旅行予定を見るを旅行計画登録済みの際のみ表示に変更

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -10,7 +10,10 @@
     @endif
     <ul class="list-disc m-5">
         <li><a>旅行計画登録</a></li>
-        <li><a>旅行予定を見る</a></li>
+        {{-- {{dd($userPlans)}} --}}
+        @if(isset($userPlans) && !$userPlans->isEmpty())
+        <li><a href="{{ route('showMyPlan', ['id' => $userPlans[0]['id']]) }}">旅行予定を見る</a></li>
+        @endif
     </ul>
     @if (isset($userPlans) && !$userPlans->isEmpty())
         <select name="userPlans">
