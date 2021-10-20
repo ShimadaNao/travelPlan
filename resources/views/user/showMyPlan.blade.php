daa2c2afcb928536cea1caeab00de7629327b148
Shimada <my.sn.0308@gmail.com>
2021-10-20 14:37:52 +0900

    showMyPlan.blade.phpで最初のプランの位置に移動するjsを実装

--- resources/views/user/showMyPlan.blade.php
+++ resources/views/user/showMyPlan.blade.php
@@ -14,12 +14,14 @@
         <link rel="stylesheet" href="/css/Control.OSMGeocoder.css" />
         <script src="{{ mix('js/Control.OSMGeocoder.js') }}"></script>
     </x-slot>
-
+    @if(session('msg'))
+        {{session('msg')}}
+    @endif
     <div class="selectMyPlan flex justify-center">
         <select name="myPlans" class="m-3">
             @foreach ($myPlans as $myPlan)
-                @if (isset($lastRegisteredPlanId))
-                    <option value="{{ $myPlan['id'] }}" {{ $lastRegisteredPlanId == $myPlan['id'] ? 'selected': ''}}>{{ $myPlan['title'] }}</option>
+                @if (isset($selectedPlan))
+                    <option value="{{ $myPlan['id'] }}" {{ $selectedPlan['id'] == $myPlan['id'] ? 'selected': ''}}>{{ $myPlan['title'] }}</option>
                 @else
                     <option value="{{ $myPlan['id'] }}">{{ $myPlan['title'] }}</option>
                 @endif
@@ -28,5 +30,45 @@
     </div>
     <div id="map">
         <script src="{{ mix('js/toppage.js') }}"></script>
+        {{-- <script src="{{ mix('js/showSelectedPlan.js') }}"></script> --}}
     </div>
 </x-header>
+<script>
+    //予定を見るをクリックしたときに1件目のプランの位置に移動
+    var firstPlan = @json($firstPlan);
+    var firstLat = firstPlan['country']['lat'];
+    var firstLng = firstPlan['country']['lng'];
+    map.setView([firstLat, firstLng]);
+
+    var selected = document.querySelector('[name="myPlans"]');
+    var countryLatLng = {};
+    selected.onchange = event => {
+        getData("/show_MyPlan/"+selected.value)
+            .then(data => {
+            //    console.log(data); // `data.json()` の呼び出しで解釈された JSON データ
+               moveToCountry(data); // 緯度と経度のデータ渡すから、マーカー処理してね
+            });
+            }
+
+    // 緯度と経度のデータをもらったのでマーカーの処理をしますね
+    var moveToCountry = function(data){
+        countryLatLng = data[1]; //国の緯度・経度をcountryLatLngに代入
+        console.log(countryLatLng);
+        //ここで移動の処理を書いていく
+        map.setView([countryLatLng["lat"], countryLatLng["lng"]]);
+    };
+    async function getData(url = '') {
+        // 既定のオプションには * が付いています
+        const response = await fetch(url, {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        })
+        return response.json(); // レスポンスの JSON を解析
+    };
+</script>
