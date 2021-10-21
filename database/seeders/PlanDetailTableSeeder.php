54bd0d9a1624690c6b589fb783249fd695983180
Shimada <my.sn.0308@gmail.com>
2021-10-21 14:41:45 +0900

    planDetailテーブルのseederファイルを追加

---
+++ database/seeders/PlanDetailTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PlanDetailTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('planDetails')->insert([
+            'id' => '1',
+            'plan_id' => '1',
+            'name' => 'ユニバーサルスタジオジャパン',
+            'latitude' => '34.66569451363396',
+            'longitude' => '135.43241500854495',
+            'dayToVisit' => '2022-04-22',
+            'timeToVisit' => '09:00:00',
+            'comment' => 'スヌーピーと写真を撮る',
+        ]);
+        DB::table('planDetails')->insert([
+            'id' => 2,
+            'plan_id' => '16',
+            'name' => 'ミラノ大聖堂',
+            'latitude' => '45.46415722165656',
+            'longitude' => '9.191651344299318',
+            'dayToVisit' => '2022-12-09',
+            'timeToVisit' => '15:00:00',
+            'comment' => 'ゆっくりと観光したい',
+        ]);
+    }
+}
