76912242c75b75f79c72153fc90f1461482e0a07
Shimada <my.sn.0308@gmail.com>
2021-11-24 13:07:56 +0900

    管理者登録完了画面のに登録された情報追加表示

--- resources/views/admin/completeRegister.blade.php
+++ resources/views/admin/completeRegister.blade.php
@@ -1,4 +1,11 @@
 <x-header>
-    <p class="flex justify-center">{{ $registerMsg }}</p>
+    <div class="adminRegisterCompleted">
+    <p>{{ $registerMsg }}</p>
+    @if($registeredData !== null)
+        <p>登録情報</p>
+        管理者：{{ $registeredData['name'] }}<br>
+        email：{{ $registeredData['email'] }}<br>
+    @endif
     <a href="{{route('adminDashboard')}}">トップページへ</a>
+    </div>
 </x-header>
