3edac7ae59def4a398420c286bfd35d66dca69fa
Shimada <my.sn.0308@gmail.com>
2021-11-25 13:03:34 +0900

    新規管理者登録の前に現ログイン管理者のパスワード再認証実装のためのjsファイルを追加

---
+++ public/js/adminPwCheck.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************************!*\
+  !*** ./resources/js/adminPwCheck.js ***!
+  \**************************************/
+window.form = '';
+window.pwInput = '';
+form = document.querySelector('.pwForm');
+var pwInput = form.querySelector('input[name="password"]');
+/******/ })()
+;
---
+++ resources/js/adminPwCheck.js
@@ -0,0 +1,4 @@
+window.form = '';
+window.pwInput = '';
+form = document.querySelector('.pwForm');
+var pwInput = form.querySelector('input[name="password"]');
