81f7233a76150f8e6796d0b452e9479117ce3593
Shimada <my.sn.0308@gmail.com>
2021-11-25 14:34:57 +0900

    新規管理者登録時の現ログイン管理者の再認証のfetch post通信の処理を追加

--- resources/js/adminPwCheck.js
+++ resources/js/adminPwCheck.js
@@ -1,4 +1,33 @@
-window.form = '';
-window.pwInput = '';
-form = document.querySelector('.pwForm');
-var pwInput = form.querySelector('input[name="password"]');
+// window.form = '';
+// window.pwInput = '';
+// var pwInput = form.querySelector('input[name="password"]');
+
+window.pwCheckPost = function(){
+    const form = document.querySelector('.pwForm');
+    const url = '/admins/reAuth';
+    let formData = new FormData(form);
+    for(let value of formData.entries()){
+        console.log(value);
+    }
+    fetch(url,{
+        method: 'POST',
+        body: formData
+    }).then((response) => {
+        if(!response.ok){
+            console.log('error!');
+        }
+        console.log('ok!');
+        return response.text();
+    }).then((data) => {
+        console.log(data);
+        //ここから確認していく run devして
+        if(data === '認証に失敗しました'){
+            alert('再度認証');
+        } else {
+            var checkForm = document.querySelector('.pwCheck');
+            checkForm.remove();
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+}
