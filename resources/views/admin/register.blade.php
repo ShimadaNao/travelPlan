84e30bb7b2b316f331e392e9362a2d160f347150
Shimada <my.sn.0308@gmail.com>
2021-11-25 12:14:10 +0900

    新規管理者登録画面にバリデーションエラーメッセージ表示の実装を追加

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -3,8 +3,29 @@
     <div class="flex justify-center">
     <form method="post" action="{{route('confirmAdminRegister')}}">
         @csrf
+        @if($errors->has('name'))
+            @foreach($errors->get('name') as $message)
+            <div class="text-red-600">
+                <li>{{ $message }}</li>
+            </div>
+            @endforeach
+        @endif
         管理者名:<input type="text" name="name"><br>
+        @if($errors->has('email'))
+            @foreach($errors->get('email') as $message)
+            <div class="text-red-600">
+                <li>{{ $message }}</li>
+            </div>
+            @endforeach
+        @endif
         email:<input type="email" name="email"><br>
+        @if($errors->has('password'))
+            @foreach($errors->get('password') as $message)
+            <div class="text-red-600">
+                <li>{{ $message }}</li>
+            </div>
+            @endforeach
+        @endif
         パスワード:<input type="password" name="password"><br>
         <div class="flex justify-center">
         <input type="submit" value="確認する">
