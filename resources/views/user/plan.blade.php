2c490b3d568a4ace2379a59a33d3657d0bfa86a8
Shimada <my.sn.0308@gmail.com>
2021-12-01 14:33:15 +0900

    計画一覧ページに計画詳細ページを表示するリンクを追加

--- resources/views/user/plan.blade.php
+++ resources/views/user/plan.blade.php
@@ -4,7 +4,7 @@
     <h3 style="color:green;">これからの旅行</h3>
         <div>
             @foreach($futurePlans as $futurePlan)
-                <a href="">{{ $futurePlan['title'] }}</a><br>
+                <a href="{{ route('planDetailPage', $futurePlan['id']) }}">{{ $futurePlan['title'] }}</a><br>
             @endforeach
         </div>
     </div>
