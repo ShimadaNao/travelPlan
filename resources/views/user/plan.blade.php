a730b42581e71d4b64da66b43ba6dac6a2a73482
Shimada <my.sn.0308@gmail.com>
2021-12-01 14:10:07 +0900

    旅行計画一覧ページのcssを調節

--- resources/views/user/plan.blade.php
+++ resources/views/user/plan.blade.php
@@ -1,7 +1,11 @@
 <x-header>
     [$futurePlans, $pastPlans, $today];
-    <h3>これからの旅行</h3>
-    @foreach($futurePlans as $futurePlan)
-        <a href="">{{ $futurePlan['title'] }}</a><br>
-    @endforeach
+    <div style="flex-flow: column; display:flex; width:30%; justify-content: center; align-items: center;">
+    <h3 style="color:green;">これからの旅行</h3>
+        <div>
+            @foreach($futurePlans as $futurePlan)
+                <a href="">{{ $futurePlan['title'] }}</a><br>
+            @endforeach
+        </div>
+    </div>
 </x-header>
