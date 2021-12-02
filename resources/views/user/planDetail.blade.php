4656bd5690fb4ec5e08b1e5e5d632cd5dfbf0661
Shimada <my.sn.0308@gmail.com>
2021-12-02 12:05:37 +0900

    計画詳細閲覧画面のbladeを追加

--- resources/views/user/planDetail.blade.php
+++ resources/views/user/planDetail.blade.php
@@ -1,3 +1,24 @@
 <x-header>
-    {{ $planInfo }}
+    <div class="detailTable">
+        <table class="table-auto">
+            <thead>
+                <tr>
+                    <th>日付</th>
+                    <th>観光地名</th>
+                    <th>予定時間</th>
+                    <th>ひとこと</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($planDetailInfo as $detail)
+                    <tr>
+                        <td>{{ $detail['dayToVisit'] }}</td>
+                        <td>{{ $detail['name'] }}</td>
+                        <td>{{ substr($detail['timeToVisit'], 0, 5) }}</td>
+                        <td>{{ $detail['comment'] }}</td>
+                    <tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </x-header>
