5def3d9d508dbf5c7f29a911d3e808fabbcb0e07
Shimada <my.sn.0308@gmail.com>
2021-12-04 14:42:41 +0900

    不要なコードを削除

--- resources/views/user/plan.blade.php
+++ resources/views/user/plan.blade.php
@@ -1,5 +1,4 @@
 <x-header>
-    [$futurePlans, $pastPlans, $today];
     <div style="flex-flow: column; display:flex; width:30%; justify-content: center; align-items: center;">
     <h3 style="color:green;">これからの旅行</h3>
         <div>
