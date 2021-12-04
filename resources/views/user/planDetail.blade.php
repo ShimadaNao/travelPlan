8c86d707114fbc5fb75a45e1523879a78b2bd657
Shimada <my.sn.0308@gmail.com>
2021-12-04 14:49:22 +0900

    計画詳細一覧画面をtableタグで表にした

--- resources/views/user/planDetail.blade.php
+++ resources/views/user/planDetail.blade.php
@@ -1,24 +1,31 @@
 <x-header>
     <div class="detailTable">
-        <table class="table-auto">
-            <thead>
-                <tr>
-                    <th>日付</th>
-                    <th>観光地名</th>
-                    <th>予定時間</th>
-                    <th>ひとこと</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($planDetailInfo as $detail)
+        @if($planDetail->isEmpty())
+            <p>まだプランが登録されていません</p>
+        @else
+            <table class="table-auto">
+                <thead>
                     <tr>
-                        <td>{{ $detail['dayToVisit'] }}</td>
-                        <td>{{ $detail['name'] }}</td>
-                        <td>{{ substr($detail['timeToVisit'], 0, 5) }}</td>
-                        <td>{{ $detail['comment'] }}</td>
-                    <tr>
-                @endforeach
-            </tbody>
-        </table>
+                        <th>日付</th>
+                        <th>観光地名</th>
+                        <th>予定時間</th>
+                        <th>ひとこと</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($planDetail as $detail)
+                        <tr>
+                            <td>{{ $detail['dayToVisit'] }}</td>
+                            <td>{{ $detail['name'] }}</td>
+                            <td>{{ substr($detail['timeToVisit'], 0, 5) }}</td>
+                            <td>{{ $detail['comment'] }}</td>
+                        <tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <div class="paginate">
+                {{ $planDetail->links() }}
+            </div>
+        @endif
     </div>
 </x-header>
