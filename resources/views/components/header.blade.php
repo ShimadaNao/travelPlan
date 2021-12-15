2bd2b10517617a6af2e5cc6e04aad308411f9056
Shimada <my.sn.0308@gmail.com>
2021-12-15 14:29:06 +0900

    ヘッダーにカレンダー表示のリンクを追加

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -37,6 +37,7 @@
                     @auth('users')
                     <li><a href="{{ route('userDashboard') }}"  class="block px-8 py-2 my-4 hover:bg-indigo-300 rounded">マップ</a></li>
                     <li><a href="{{ route('planCharts') }}"  class="block px-8 py-2 my-4 hover:bg-indigo-300 rounded">計画予定</a></li>
+                    <li><a href="{{ route('calendar') }}" class="block px-8 py-2 my-4 hover:bg-indigo-300 rounded">カレンダー</a></li>
                     <li><a href="/logout"  class="block px-8 py-2 my-4 hover:bg-indigo-300 rounded">ログアウト</a></li>
                     @endauth
                     @guest
