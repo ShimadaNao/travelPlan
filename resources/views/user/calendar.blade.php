755d7650e57a1f6e3cbbe5ea6ae3739dc25d38fb
Shimada <my.sn.0308@gmail.com>
2021-12-27 13:54:18 +0900

    カレンダーのcssを調整・スケジュール表示ボタンを追加

--- resources/views/user/calendar.blade.php
+++ resources/views/user/calendar.blade.php
@@ -1,5 +1,5 @@
 <x-header>
-    <div class="wrapper">
+    <div class="wrapper" style="text-align: center;">
         <!-- xxxx年xx月を表示 -->
         <h1 id="header"></h1>
     
@@ -10,10 +10,11 @@
         </div>
     
         <!-- カレンダー -->
-        <div id="calendar"></div>
+        <div id="calendar"  style="text-align: center;"></div>
         <script>
             window.myPlans = @json($myPlans);
         </script>
+        <button id="button">スケジュールを表示</button>
         <script src="{{ mix('js/calendar.js') }}"></script>
     </div>
 </x-header>
