41c1bcaffb91199a9dc241530a038c317180e367
Shimada <my.sn.0308@gmail.com>
2021-12-28 12:11:36 +0900

    旅行計画表ページのデザインを修正

--- resources/views/user/planChart.blade.php
+++ resources/views/user/planChart.blade.php
@@ -1,19 +1,22 @@
 <x-header>
-    <div style="display: flex; justify-content: space-around; margin: 0 20%;">{{-- このdivに justify-content: space-around;で子要素のjustfy-contentをなくしても良いかも --}}
-        <div style="flex-flow: column; text-align: center;">
-        <h3 style="color:green;">これからの旅行</h3>
-            <div>
-                @foreach($futurePlans as $futurePlan)
-                    <a href="{{ route('planChartDetails', $futurePlan['id']) }}">{{ $futurePlan['title'] }}</a><br>
-                @endforeach
+    <div class="planChart-wrapper">
+        <div class="planChart-body">
+        {{-- style="display: flex; justify-content: space-around; margin: 0 20%;"> --}}
+            <div style="flex-flow: column; text-align: center;">
+            <h3>これからの旅行</h3>
+                <div>
+                    @foreach($futurePlans as $futurePlan)
+                        <a href="{{ route('planChartDetails', $futurePlan['id']) }}">{{ $futurePlan['title'] }}</a><br>
+                    @endforeach
+                </div>
             </div>
-        </div>
-        <div style="flex-flow: column; text-align: center;">
-            <h3 style="color: green">過去の旅行</h3>
-            <div>
-                @foreach($pastPlans as $pastPlan)
-                    <a href="{{ route('planChartDetails', $pastPlan['id']) }}">{{ $pastPlan['title'] }}</a><br>
-                @endforeach
+            <div style="flex-flow: column; text-align: center;">
+                <h3>過去の旅行</h3>
+                <div>
+                    @foreach($pastPlans as $pastPlan)
+                        <a href="{{ route('planChartDetails', $pastPlan['id']) }}">{{ $pastPlan['title'] }}</a><br>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
