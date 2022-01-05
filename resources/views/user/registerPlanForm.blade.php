8de108e0a9540d4ae8d73d8b972635ded4db7417
Shimada <my.sn.0308@gmail.com>
2022-01-05 14:23:26 +0900

    旅行名登録フォームにバリデーションエラーメッセージを追加

--- resources/views/user/registerPlanForm.blade.php
+++ resources/views/user/registerPlanForm.blade.php
@@ -3,14 +3,37 @@
         <h3>旅行予定登録フォームです</h3>
         <form method="post" action="{{ route('registerTravelTitle') }}">
             @csrf
+            @if($errors->has('title'))
+                @foreach($errors->get('title') as $error)
+                    <p style="color:orangered">{{ $error }}<p>
+                @endforeach
+            @endif
             旅行名：<input typw="text" name="title"><br>
+
+            @if($errors->has('country'))
+            @foreach($errors->get('country') as $error)
+                <p style="color:orangered">{{ $error }}<p>
+            @endforeach
+            @endif
             国名：
             <select name="country">
                 @foreach($countries as $country)
                 <option value="{{ $country['id'] }}">{{ $country['nameJP']}}</option>
                 @endforeach
             </select><br>
+
+            @if($errors->has('start'))
+            @foreach($errors->get('start') as $error)
+                <p style="color:orangered">{{ $error }}<p>
+            @endforeach
+            @endif
             旅行開始日<input type="date" name="start"><br>
+
+            @if($errors->has('end'))
+            @foreach($errors->get('end') as $error)
+                <p style="color:orangered">{{ $error }}<p>
+            @endforeach
+            @endif
             旅行終了日：<input type="date" name="end"><br>
             <input type="submit" value="送信">
         </form>
