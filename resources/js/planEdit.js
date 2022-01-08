70c9ef864d9ac2c665f4f6d9e4dfd4321035c24d
Shimada <my.sn.0308@gmail.com>
2022-01-08 11:43:45 +0900

    計画詳細一覧表ページで編集ボタンクリックでinputタグに書き換える実装を追加

--- resources/js/planEdit.js
+++ resources/js/planEdit.js
@@ -1,3 +1,16 @@
-console.log('success');
-//編集ボタンがクリックされたら、タイトル・日程をinputタグにし編集可能とする。
-window.editBtn = document.querySelector("a[id='edit']");
+//編集ボタンがクリックされたら、タイトル・日程をinputタグにし編集可能に
+window.editBtn = document.querySelector('#editBtn');
+window.planChartTitle = document.querySelector('.planChartTitle');
+
+window.editTitle = function() {
+    var title = document.querySelector('#planTitle');
+    var date = document.querySelector('#planDate');
+    window.planChartTitle.innerHTML = "<p style='font-size: 30px; color: violet;'>編集画面中です</p>"
+    + "<form method='post' action=''>タイトル：<input type='text' name='title' value='"
+    + planTitle + "'><br />旅行開始日：<input type='date' value='" 
+    + planStart + "'><br />旅行終了日：<input type='date' value='"
+    + planEnd + "'><input type='hidden' name='plan_id' value='" 
+    + planId + "'><br /><input type='submit' value='更新'></form>";
+}
+
+window.editBtn.onclick = window.editTitle;
