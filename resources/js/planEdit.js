c5f732cb01f2d56c2fdc5410f2a0444ba7ce69b2
Shimada <my.sn.0308@gmail.com>
2022-01-08 14:58:26 +0900

    旅行名更新の際のfetch通信の実装をplanEdit.jsに追加

--- resources/js/planEdit.js
+++ resources/js/planEdit.js
@@ -1,16 +1,40 @@
 //編集ボタンがクリックされたら、タイトル・日程をinputタグにし編集可能に
 window.editBtn = document.querySelector('#editBtn');
 window.planChartTitle = document.querySelector('.planChartTitle');
+window.csrf_token = document.querySelector('meta[name="csrf-token"]').content;
 
 window.editTitle = function() {
     var title = document.querySelector('#planTitle');
     var date = document.querySelector('#planDate');
     window.planChartTitle.innerHTML = "<p style='font-size: 30px; color: violet;'>編集画面中です</p>"
-    + "<form method='post' action=''>タイトル：<input type='text' name='title' value='"
-    + planTitle + "'><br />旅行開始日：<input type='date' value='" 
-    + planStart + "'><br />旅行終了日：<input type='date' value='"
+    + "<form class='updateForm'>タイトル：<input type='text' name='title' value='"
+    + planTitle + "'><br />旅行開始日：<input type='date' name='start' value='" 
+    + planStart + "'><br />旅行終了日：<input type='date' name='end' value='"
     + planEnd + "'><input type='hidden' name='plan_id' value='" 
-    + planId + "'><br /><input type='submit' value='更新'></form>";
+    + planId + "'><input type='hidden' name='_token' value='"
+    + csrf_token + "'><br /><input type='button' value='送信' onclick='window.updatePlan()'></form>";
 }
-
 window.editBtn.onclick = window.editTitle;
+
+window.updatePlan = function(){
+    const postFetchForm = document.querySelector('.updateForm');
+    const url = '/users/updatePlan';
+    let formData = new FormData(postFetchForm);
+    for(let value of formData.entries()) {
+        console.log(value);
+    }
+    fetch(url, {
+        method: "POST",
+        body: formData
+    }).then((response) => {
+        if(!response.ok) {
+            console.log('error!');
+        }
+        console.log('ok!');
+        return response.text();
+    }).then((data) => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    });
+}
