ade1f2425fd8d4363d76b0b8e4ebd7b5d6fe268d
Shimada <my.sn.0308@gmail.com>
2022-01-14 12:00:50 +0900

    旅行名編集フォームのデザインを修正

--- resources/js/planEdit.js
+++ resources/js/planEdit.js
@@ -7,10 +7,10 @@ window.editTitle = function() {
     var title = document.querySelector('#planTitle');
     var date = document.querySelector('#planDate');
     var editForm = "<p style='font-size: 30px; color: violet;'>編集画面中です</p>"
-    + "<form class='updateForm'>タイトル：<input type='text' name='title' value='"
-    + planTitle + "'><br />旅行開始日：<input type='date' name='start' value='" 
-    + planStart + "'><br />旅行終了日：<input type='date' name='end' value='"
-    + planEnd + "'><input type='hidden' name='plan_id' value='" 
+    + "<form class='updateForm'><dl><dt>タイトル：</dt><dd><input type='text' name='title' value='"
+    + planTitle + "'></dd></dl><dl><dt>旅行開始日：</dt><dd><input type='date' name='start' value='" 
+    + planStart + "'></dd></dl><dl><dt>旅行終了日：</dt><dd><input type='date' name='end' value='"
+    + planEnd + "'></dd></dl><input type='hidden' name='plan_id' value='" 
     + planId + "'><input type='hidden' name='_token' value='"
     + csrf_token + "'><br /><input type='button' value='送信' onclick='window.updatePlan()'></form>";
     window.planChartTitle.innerHTML = editForm;
