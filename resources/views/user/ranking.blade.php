28e205c5afcab666f1aa3a64bb690db637e52c28
Shimada <my.sn.0308@gmail.com>
2022-01-22 11:45:40 +0900

    ランキングページ(ranking.blade.php)にパーセント表示を追加

--- resources/views/user/ranking.blade.php
+++ resources/views/user/ranking.blade.php
@@ -5,7 +5,10 @@
         <dl>
         <ul class="list-disc">
             @for($i = 0; $i<count($ranking); $i++)
-            <li><label class="country">{{ $i+1 }}位 {{ $ranking[$i]['country']['nameJP']}}</label><span class="country_count">{{$ranking[$i]['country_count']}}件</span></li>
+            <li><label class="country">{{ $i+1 }}位 {{ $ranking[$i]['country']['nameJP']}}</label>
+                <span class="country_count">{{$ranking[$i]['country_count']}}件</span>
+                <div class="percentage">({{ floor($ranking[$i]['country_count']/ $denominator * 100) }}％)</div>
+            </li>
             @endfor
         </ul>
         </dl>
