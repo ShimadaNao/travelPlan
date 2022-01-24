4f6bbd03144922238054f5de76269efbf9c3bd8c
Shimada <my.sn.0308@gmail.com>
2022-01-24 11:32:39 +0900

    calendar.bladeにヘッダー用のx-slotタグを追加

--- resources/views/user/calendar.blade.php
+++ resources/views/user/calendar.blade.php
@@ -1,4 +1,12 @@
 <x-header>
+    <x-slot name="header">
+        <li><a href="{{ route('userDashboard') }}" class="headerNav">マップ</a></li>
+        <li><a href="{{ route('registerPlanForm') }}" class="headerNav">旅行計画登録フォーム</a></li>
+        <li><a href="{{ route('planCharts') }}" class="headerNav">計画予定表</a></li>
+        <li><a href="{{ route('calendar') }}" class="headerNav selec">カレンダー</a></li>
+        <li><a href="{{ route('countryRanking') }}"  class="headerNav">人気国</a></li>
+        <li><a href="/logout" class="headerNav">ログアウト</a></li>
+    </x-slot>
     <div class="wrapper" style="text-align: center;">
         <!-- xxxx年xx月を表示 -->
         <h1 id="header"></h1>
