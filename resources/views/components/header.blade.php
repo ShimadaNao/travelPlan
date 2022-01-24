32523979fc4ff8479a64b89f6da9a728de9d5d8e
Shimada <my.sn.0308@gmail.com>
2022-01-24 11:28:26 +0900

    ヘッダーでBladeComponent使用するためユーザー用ヘッダー内を{{$header}}に変更

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -35,12 +35,7 @@
                 <nav>
                 <ul class="flex justify-center">
                     @auth('users')
-                    <li><a href="{{ route('userDashboard') }}" class="headerNav">マップ</a></li>
-                    <li><a href="{{ route('registerPlanForm') }}" class="headerNav">旅行計画登録フォーム</a></li>
-                    <li><a href="{{ route('planCharts') }}" class="headerNav">計画予定表</a></li>
-                    <li><a href="{{ route('calendar') }}" class="headerNav">カレンダー</a></li>
-                    <li><a href="{{ route('countryRanking') }}"  class="headerNav">人気国</a></li>
-                    <li><a href="/logout" class="headerNav">ログアウト</a></li>
+                    {{ $header }}
                     @endauth
                     @guest
                     <li><a href="{{ route('multi_login') }}"  class="headerNav">ログイン</a></li>
