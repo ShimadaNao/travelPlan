e06bda674064663d91f1f218af3acfbeca946a54
Shimada <my.sn.0308@gmail.com>
2022-02-05 13:50:46 +0900

    [admin]旅行検索画面時のバリデーションのためのフォームリクエストを追加

---
+++ app/Http/Requests/planSearchRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class planSearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        // どちらかのみでもokのためrequired_withoutを使用
+        return [
+            'plan_id' => 'required_without:keyword',
+            'keyword' => '',
+        ];
+    }
+}
