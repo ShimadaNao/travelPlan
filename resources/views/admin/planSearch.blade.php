d6666e65a9e65b5f021d91e14c4bac82c9e637da
Shimada <my.sn.0308@gmail.com>
2022-02-05 11:56:49 +0900

    [admin]旅行検索画面のviewを作成

---
+++ resources/views/admin/planSearch.blade.php
@@ -0,0 +1,9 @@
+<x-header>
+    <div class="flex flex-col items-center text-center p-4">
+        <p class="text-pink-400 p-8 text-2xl">旅行検索画面です</p>
+        <form method="post" action="">
+            <p>旅行Idを入力してください</p>
+            <input type="text" name="plan_id" placeholder="旅行ID">
+            <input type="submit" value="検索する" class="mt-4">
+        </form>
+</x-header>
