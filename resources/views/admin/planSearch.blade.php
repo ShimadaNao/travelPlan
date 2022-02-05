5df7e96609f2d6ea02fac41115e0dd9be6d4e57e
Shimada <my.sn.0308@gmail.com>
2022-02-05 13:50:09 +0900

    旅行検索画面のviewを修正

--- resources/views/admin/planSearch.blade.php
+++ resources/views/admin/planSearch.blade.php
@@ -1,9 +1,11 @@
 <x-header>
     <div class="flex flex-col items-center text-center p-4">
         <p class="text-pink-400 p-8 text-2xl">旅行検索画面です</p>
-        <form method="post" action="">
+        <form method="get" action="{{ route('planSearchResult') }}">
             <p>旅行Idを入力してください</p>
-            <input type="text" name="plan_id" placeholder="旅行ID">
+            <input type="text" name="plan_id" placeholder="旅行ID"><br>
+            <p>キーワードを入力してください</p>
+            <input type="text" name="keyword" planceholder="キーワード"><br>
             <input type="submit" value="検索する" class="mt-4">
         </form>
 </x-header>
