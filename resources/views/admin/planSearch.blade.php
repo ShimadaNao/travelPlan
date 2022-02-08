196c851ea8914fe69f0c245a67284ce70824b6c3
Shimada <my.sn.0308@gmail.com>
2022-02-08 15:05:52 +0900

    [admin]旅行検索画面にバリデーションエラーメッセージ表示の実装を追加

--- resources/views/admin/planSearch.blade.php
+++ resources/views/admin/planSearch.blade.php
@@ -3,8 +3,18 @@
         <p class="text-pink-400 p-8 text-2xl">旅行検索画面です</p>
         <form method="get" action="{{ route('planSearchResult') }}">
             <p>旅行Idを入力してください</p>
+            @if($errors->has('plan_id'))
+                @foreach ($errors->get('plan_id') as $message)
+                    {{ $message }}
+                @endforeach
+            @endif
             <input type="text" name="plan_id" placeholder="旅行ID"><br>
             <p>キーワードを入力してください</p>
+            @if($errors->has('keyword'))
+            @foreach ($errors->get('keyword') as $message)
+                {{ $message }}<br>
+            @endforeach
+        @endif
             <input type="text" name="keyword" planceholder="キーワード"><br>
             <input type="submit" value="検索する" class="mt-4">
         </form>
