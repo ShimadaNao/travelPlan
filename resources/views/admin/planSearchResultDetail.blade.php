11685bade27c498b35b7a0723d8efc4a72f25702
Shimada <my.sn.0308@gmail.com>
2022-02-10 14:29:10 +0900

    [admin]旅行検索結果詳細画面のデザインを調整

--- resources/views/admin/planSearchResultDetail.blade.php
+++ resources/views/admin/planSearchResultDetail.blade.php
@@ -1,9 +1,28 @@
 <x-header>
-    <p>検索結果詳細画面です</p>
-    タイトル：{{ $planDetail['title'] }}<br>
-    ユーザーID：{{ $planDetail['user_id'] }}<br>
-    ユーザー名：{{ $planDetail['user']['name'] }}<br>
-    開始日：{{ $planDetail['start'] }}<br>
-    終了日：{{ $planDetail['end'] }}<br>
-    <button style="background-color:burlywood; border-radius: 5px; " onclick="location.href='/admins/dashboard'">トップページへ</button>
+    <div class="searchResultWrapper">
+        <h1 class="planSearchResultTitle">検索結果詳細画面です</h1>
+        <div class="resultDetailTable">
+            <div class="detailTableRow">
+                <label class="detailTableItem">タイトル：</label>
+                <label class="detailTableItem">{{ $planDetail['title'] }}</label>
+            </div>
+            <div class="detailTableRow">
+                <label class="detailTableItem">ユーザーID：</label>
+                <label class="detailTableItem">{{ $planDetail['user_id'] }}</label>
+            </div>
+            <div class="detailTableRow">
+                <label class="detailTableItem">ユーザー名：</label>
+                <label class="detailTableItem">{{ $planDetail['user']['name'] }}</label>
+            </div>
+            <div class="detailTableRow">
+                <label class="detailTableItem">開始日：</label>
+                <label class="detailTableItem">{{ $planDetail['start'] }}</label>
+            </div>
+            <div class="detailTableRow">
+                <label class="detailTableItem">終了日：</label>
+                <label class="detailTableItem">{{ $planDetail['end'] }}</label>
+            </div>
+        </div> 
+        <button class="detailTableBtn" onclick="location.href='/admins/dashboard'">トップページへ</button>
+    </div>
 </x-header>
