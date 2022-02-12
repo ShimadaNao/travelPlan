56d938284a9962831af9c588e8e4b543fe584d4c
Shimada <my.sn.0308@gmail.com>
2022-02-12 11:19:27 +0900

    Planモデルに旅行公開非公開設定のpublicカラムを追加したのでseederファイルも変更

--- database/seeders/PlanTableSeeder.php
+++ database/seeders/PlanTableSeeder.php
@@ -19,22 +19,45 @@ class PlanTableSeeder extends Seeder
             'user_id' => 1,
             'country_id' => 153,
             'title' => '大阪旅行',
-            // 'city' => '大阪',
-            // 'latitude' => '34.6778',
-            // 'longitude' => '135.4546',
             'start' => '2022-04-22',
             'end' => '2022-04-28',
+            'public' => 'yes',
         ]);
         DB::table('plans')->insert([
             'id' => 2,
             'user_id' => 2,
             'country_id' => 5,
             'title' => 'アメリカ旅行',
-            // 'city' => 'ロサンゼルス',
-            // 'latitude' => '33.9991',
-            // 'longitude' => '118.4117',
             'start' => '2022-11-08',
             'end' => '2022-11-15',
+            'public' => 'yes',
+        ]);
+        DB::table('plans')->insert([
+            'id' => 3,
+            'user_id' => 2,
+            'country_id' => 208,
+            'title' => '香港旅行',
+            'start' => '2022-02-08',
+            'end' => '2022-02-11',
+            'public' => 'no',
+        ]);
+        DB::table('plans')->insert([
+            'id' => 4,
+            'user_id' => 1,
+            'country_id' => 51,
+            'title' => 'カナダ旅行',
+            'start' => '2022-03-10',
+            'end' => '2022-03-20',
+            'public' => 'no',
+        ]);
+        DB::table('plans')->insert([
+            'id' => 5,
+            'user_id' => 1,
+            'country_id' => 156,
+            'title' => 'ニュージーランド旅行',
+            'start' => '2022-03\1-10',
+            'end' => '2022-01-23',
+            'public' => 'no',
         ]);
     }
 }
