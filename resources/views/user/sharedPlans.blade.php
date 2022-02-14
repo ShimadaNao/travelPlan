7beb9025da758b11c00b5083d8b719dd1c80d166
Shimada <my.sn.0308@gmail.com>
2022-02-14 13:58:57 +0900

    公開設定の旅行計画一覧画面にコンポーネント化したナビゲーション部分を追加

--- resources/views/user/sharedPlans.blade.php
+++ resources/views/user/sharedPlans.blade.php
@@ -1,4 +1,12 @@
 <x-header>
+    <x-slot name="header">
+        <li><a href="{{ route('userDashboard') }}" class="headerNav">マップ</a></li>
+        <li><a href="{{ route('registerPlanForm') }}" class="headerNav">旅行計画登録フォーム</a></li>
+        <li><a href="{{ route('planCharts') }}" class="headerNav">計画予定表</a></li>
+        <li><a href="{{ route('calendar') }}" class="headerNav">カレンダー</a></li>
+        <li><a href="{{ route('countryRanking') }}"  class="headerNav">人気国</a></li>
+        <li><a href="/logout" class="headerNav">ログアウト</a></li>
+    </x-slot>
     <table>
         <tr>
             <th>旅行地</th>
@@ -13,7 +21,7 @@
         </tr>
         @endforeach
     </table>
-    <div class="paginate">
+    <div class="flex">
         {{ $sharedPlans->links() }}
     </div>
 </x-header>
