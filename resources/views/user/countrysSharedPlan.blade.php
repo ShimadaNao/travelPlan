6acfc731fbe5e69d8a8cf0a361fc4eede16d730a
Shimada <my.sn.0308@gmail.com>
2022-02-17 13:09:42 +0900

    公開旅行計画国別表示画面(countrysSharedPlan)のビューを作成

---
+++ resources/views/user/countrysSharedPlan.blade.php
@@ -0,0 +1,33 @@
+<x-header>
+    <x-slot name="header">
+        <li><a href="{{ route('userDashboard') }}" class="headerNav">マップ</a></li>
+        <li><a href="{{ route('registerPlanForm') }}" class="headerNav">旅行計画登録フォーム</a></li>
+        <li><a href="{{ route('planCharts') }}" class="headerNav">計画予定表</a></li>
+        <li><a href="{{ route('calendar') }}" class="headerNav">カレンダー</a></li>
+        <li><a href="{{ route('countryRanking') }}"  class="headerNav">人気国</a></li>
+        <li><a href="/logout" class="headerNav">ログアウト</a></li>
+    </x-slot>
+    <div class="sharedPlansList">
+        <p>{{ $countryName }}の公開旅行計画一覧画面です</p>
+        <table>
+            <tr>
+                {{-- <th class="border px-4 py-2">旅行地</th> --}}
+                <th class="border px-4 py-2">旅行タイトル</th>
+                <th class="border px-4 py-2">ユーザー名</th>
+                <th class="border px-4 py-2">日程</th>
+            </tr>
+            @for($i = 0; $i < count($sharedPlans) && $i <30; $i++)
+            <tr>
+                {{-- $sharedPlans[$i]['id']でその旅行計画が取得できる --}}
+                {{-- <td class="border px-4 py-2">{{ $plan['country']['nameJP'] }}</td> --}}
+                <td class="border px-4 py-2">{{ $sharedPlans[$i]['title'] }}</td>
+                <td class="border px-4 py-2">{{ $sharedPlans[$i]['user']['name'] }}</td>
+                <td class="border px-4 py-2">{{ $sharedPlans[$i]['start'] }}～{{ $sharedPlans[$i]['end'] }}</td>
+            </tr>
+            @endfor
+        </table>
+        {{-- <div class="paginate flex">
+            {{ $sharedPlans->links() }}
+        </div> --}}
+    </div>
+</x-header>
