d241cab2092ee4132a06cf9a15cd406d40aecf4c
Shimada <my.sn.0308@gmail.com>
2022-02-23 14:45:56 +0900

    旅行名がクリックされた時にその旅行詳細テーブルを表示するjsコードを追加

--- resources/js/sharedPlan.js
+++ resources/js/sharedPlan.js
@@ -1,1 +1,34 @@
-console.log('hello');
+// window.show = function(i){
+//     console.log(planDetail[i]['plan_detail']);
+// }
+// function show(i){
+//     console.log(i);
+
+// }
+window.position = '';
+window.position_contents = '';
+position = document.querySelector('.position');
+position_contents = '<table><tr><th>場所</th><th>日程</th><th>時間</th></tr>';
+// window.new_ele = document.createElement('table');
+// window.new_tr = document.createElement('tr');
+
+// position.insertBefore(new_tr, position.firstChild);
+// new_ele.appendChild(new_tr);
+// window.new_ele_text = document.createTextNode('<tr><th>場所</th><th>日程</th><th>時間</th></tr>');
+// window.test = new_ele.appendChild(new_ele_text);
+
+// position.insertBefore(new_ele, position.firstChild);
+
+
+window.show = function(i){
+    position.style.display = "flex";
+    planDetail[i]['plan_detail'].forEach(function(ele){
+        // text += '<tr><td>' + ele.name + '</td><td>' + ele.dayToVisit + '</td><td>' + ele.timeToVisit + '</td></tr>';
+        position_contents += '<tr><td>' + ele.name + '</td><td>' + ele.dayToVisit + '</td><td>' + ele.timeToVisit + '</td></tr>';
+    });
+    position_contents += '</table>';
+    position.innerHTML = position_contents;
+    position.classList.add('position_show');
+    var table = document.getElementsByTagName('table')[0];
+    table.classList.add('publicPlanDetailTable');
+};
