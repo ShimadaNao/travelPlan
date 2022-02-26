78b1c6c68963aa6a12a26b32e6e54016e9ddc50b
Shimada <my.sn.0308@gmail.com>
2022-02-26 14:43:32 +0900

    公開旅行詳細モーダルを表示するjsコードを追加

--- public/js/sharedPlan.js
+++ public/js/sharedPlan.js
@@ -3,34 +3,52 @@ var __webpack_exports__ = {};
 /*!************************************!*\
   !*** ./resources/js/sharedPlan.js ***!
   \************************************/
-// window.show = function(i){
-//     console.log(planDetail[i]['plan_detail']);
-// }
-// function show(i){
-//     console.log(i);
-// }
 window.position = '';
 window.position_contents = '';
 position = document.querySelector('.position');
-position_contents = '<table><tr><th>場所</th><th>日程</th><th>時間</th></tr>'; // window.new_ele = document.createElement('table');
-// window.new_tr = document.createElement('tr');
-// position.insertBefore(new_tr, position.firstChild);
-// new_ele.appendChild(new_tr);
-// window.new_ele_text = document.createTextNode('<tr><th>場所</th><th>日程</th><th>時間</th></tr>');
-// window.test = new_ele.appendChild(new_ele_text);
-// position.insertBefore(new_ele, position.firstChild);
+window.cross = document.getElementsByClassName('cross')[0];
 
 window.show = function (i) {
   position.style.display = "flex";
-  planDetail[i]['plan_detail'].forEach(function (ele) {
-    // text += '<tr><td>' + ele.name + '</td><td>' + ele.dayToVisit + '</td><td>' + ele.timeToVisit + '</td></tr>';
-    position_contents += '<tr><td>' + ele.name + '</td><td>' + ele.dayToVisit + '</td><td>' + ele.timeToVisit + '</td></tr>';
-  });
-  position_contents += '</table>';
-  position.innerHTML = position_contents;
-  position.classList.add('position_show');
-  var table = document.getElementsByTagName('table')[0];
-  table.classList.add('publicPlanDetailTable');
+  position_contents = '<svg class="cross h-6 w-6 fill-current" viewBox="0 0 24 24"><path v-show="isOpen" d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" fill="black"/></svg>';
+
+  if (planDetail[i]['plan_detail'].length > 0) {
+    position_contents += '<table class="publicPlanDetailTable"><caption class="caption">' + planDetail[i]['title'] + '</caption><tr><th class="w-1/2 px-4 py-2">場所</th><th class="w-1/2 px-4 py-2">日程</th><th class="w-1/2 px-4 py-2">時間</th></tr>';
+    planDetail[i]['plan_detail'].forEach(function (ele) {
+      position_contents += '<tr><td class="border px-4 py-2 .border-white">' + ele.name + '</td><td class="border px-4 py-2 .border-white">' + ele.dayToVisit + '</td><td class="border px-4 py-2 .border-white">' + ele.timeToVisit + '</td></tr>';
+    });
+    position_contents += '</table>';
+    position.innerHTML = position_contents;
+    position.classList.add('position_show');
+    var table = document.getElementsByTagName('table')[0];
+    table.classList.add('publicPlanDetailTable');
+    window.getCross();
+    cross.onclick = crossClick;
+  } else {
+    position_contents += '<p class="caption">' + planDetail[i]['title'] + '</p><p>表示するコンテンツがありません</p>';
+    position.classList.add('publicNoDetail');
+    position.innerHTML = position_contents;
+    window.getCross();
+    cross.onclick = crossClick;
+  }
+};
+
+window.getCross = function () {
+  cross = document.getElementsByClassName('cross')[0];
+  cross.classList.add('showCross');
+};
+
+window.crossClick = function () {
+  if (position.style.display == 'flex') {
+    position.style.display = 'none';
+    position_contents = '';
+
+    if (position.classList.contains('position_show')) {
+      position.classList.remove('position_show');
+    } else {
+      position.classList.remove('publicNoDetail');
+    }
+  }
 };
 /******/ })()
 ;
