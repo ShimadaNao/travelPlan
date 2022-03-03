785042d486bcd5805defa85083c6a859461e466d
Shimada <my.sn.0308@gmail.com>
2022-03-03 14:24:12 +0900

    問い合わせテーブル変更に伴いseederファイルも変更(InquiryTableSeeder)

--- database/seeders/InquiryTableSeeder.php
+++ database/seeders/InquiryTableSeeder.php
@@ -19,19 +19,17 @@ class InquiryTableSeeder extends Seeder
             'id' => 1,
             'user_id' => 1,
             'genre_id' => 1,
+            'answer_id' => 1,
             'title' => '旅行計画登録について',
             'content' => '旅行計画登録は旅行計画登録フォームからのみ追加可能なのでしょうか？',
-            'answerer_id' => 1,
-            'answer' => 'トップページからも登録することができます',
         ]);
         DB::table('inquiries')->insert([
             'id' => 2,
             'user_id' => 2,
             'genre_id' => 2,
+            'answer_id' => null,
             'title' => '会員登録について',
             'content' => '会員登録には料金がかかりますか？',
-            'answerer_id' => 1,
-            'answer' => '料金は無料です',
         ]);
     }
 }
