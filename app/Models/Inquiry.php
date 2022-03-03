0208b6b2ab82b0b71a1bc27c3b5adb571d669a18
Shimada <my.sn.0308@gmail.com>
2022-03-03 14:46:41 +0900

    問い合わせモデル(inquiry.php)にリレーションを追加

--- app/Models/Inquiry.php
+++ app/Models/Inquiry.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Inquiry extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function inquiryGenre()
+    {
+        return $this->belongsTo(InquiryGenre::class);
+    }
+
+    public function inquiryAnswer()
+    {
+        return $this->hasOne(inquiryAnswer::class);
+    }
 }
