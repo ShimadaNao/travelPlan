c89146c25907dfc87373a08f03766f13ff214c0a
Shimada <my.sn.0308@gmail.com>
2022-03-03 13:56:54 +0900

    お問い合わせのテーブル(Inquiriesテーブル)のマイグレーションファイルを作成

---
+++ database/migrations/2022_03_03_025150_create_inquiries_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('genre_id');
+            $table->string('title');
+            $table->string('content');
+            $table->integer('answerer_id')->nullable();
+            $table->string('answer')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            
+            $table->foreign('genre_id')
+            ->references('id')
+            ->on('inquiryGenres')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inquiries');
+    }
+}
