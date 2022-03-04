f4d0b96213f271455625309061543ef514015337
Shimada <my.sn.0308@gmail.com>
2022-03-04 14:29:33 +0900

    問い合わせ内容確認画面表示のルートを追加

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::prefix('users')->middleware('auth:users')->group(function () {
     Route::get('sharedPlans', [MapController::class, 'showSharedPlans'])->name('sharedPlans');
     Route::get('sharedPlansCountry/{id}', [MapController::class, 'showItsSharedPlans'])->name('itsSharedPlan');
     Route::get('inquiry', [MapController::class, 'showInquiryForm'])->name('showInquiryForm');
-    // Route::post('confirmInquiry', [MapController::class, 'confinmInquiry'])->name('confirmInquiry');
+    Route::post('confirmInquiry', [MapController::class, 'confirmInquiry'])->name('confirmInquiry');
 });
 Route::prefix('admins')->middleware('auth:admins')->group(function() {
     Route::get('register', [MultiAuthController::class, 'registerAdmin'])->name('registerAdmin');
