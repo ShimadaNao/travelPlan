38d0d52ac4d3dcc80008abb9e35ce60607132b6c
Shimada <my.sn.0308@gmail.com>
2022-03-04 10:57:40 +0900

    問い合わせフォーム表示のルートを追加

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,8 @@ Route::prefix('users')->middleware('auth:users')->group(function () {
     Route::get('ranking', [MapController::class, 'showPopularCountryRanking'])->name('countryRanking');
     Route::get('sharedPlans', [MapController::class, 'showSharedPlans'])->name('sharedPlans');
     Route::get('sharedPlansCountry/{id}', [MapController::class, 'showItsSharedPlans'])->name('itsSharedPlan');
-    Route::view('inquiry', 'user.inquiry')->name('showInquiryForm');
+    Route::get('inquiry', [MapController::class, 'showInquiryForm'])->name('showInquiryForm');
+    // Route::post('confirmInquiry', [MapController::class, 'confinmInquiry'])->name('confirmInquiry');
 });
 Route::prefix('admins')->middleware('auth:admins')->group(function() {
     Route::get('register', [MultiAuthController::class, 'registerAdmin'])->name('registerAdmin');
