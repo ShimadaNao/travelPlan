1b7a04377502eb5768bd27831ba4eef340d6a400
Shimada <my.sn.0308@gmail.com>
2022-03-05 15:53:03 +0900

    Inquiryモデルに問い合わせ内容をDBに登録するメソッドを追加(completeInquiry)

--- app/Models/Inquiry.php
+++ app/Models/Inquiry.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Services\InquiryService;
 
 class Inquiry extends Model
 {
@@ -26,5 +27,25 @@ class Inquiry extends Model
     public function inquiryAnswer()
     {
         return $this->hasOne(inquiryAnswer::class);
+    }
+
+    // public function __construct(InquiryService $inquiryService)
+    // {
+    //     $this->inquiryService = $inquiryService;
+    // }
+
+    public function completeInquiry($request)
+    {
+        // この辺の処理はサービスに移してよさそう
+        $this->inquiryService = app()->make(InquiryService::class);
+        $data = $this->inquiryService->getInquiryForm($request);
+        // dd($data);
+        $result = Inquiry::firstOrCreate($data);
+        if($result->wasRecentlyCreated)
+        {
+            return $message = 'お問い合わせを受け付けました';
+        }else {
+            return $message = 'お問い合わせを受け付けられませんでした。再度お試し下さい。';
+        }
     }
 }
