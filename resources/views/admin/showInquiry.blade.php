1466813fbc0ea9eda20b5c9489feb5c741ba50c6
Shimada <my.sn.0308@gmail.com>
2022-03-09 15:04:12 +0900

    管理者の問い合わせ一覧ページのビューを作成(admin.showInquiry.blade)

--- resources/views/admin/showInquiry.blade.php
+++ resources/views/admin/showInquiry.blade.php
@@ -1,3 +1,21 @@
 <x-header>
 {{-- $waiting $doneで出せるプルダウンで表示切替したい --}}
+<select name="answeredOrNot">
+    <option value="1" selected>未回答</option>
+    <option value="0">回答済み</option>
+</select>
+<div class="inquiryArea" style="padding: 20px;">
+    @foreach($waitings as $waiting)
+    <ul>
+    <a href="#">
+        <li class="list-disc">
+            ID：{{ $waiting['id'] }}
+            タイトル：{{ $waiting['title'] }}<br>
+        </li>
+    </a>
+    @endforeach
+</div>
+{{-- {{dd($waiting);}} --}}
+
+
 </x-header>
