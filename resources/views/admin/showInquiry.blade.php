92c5133701dbbf48f8e1e69f2786b5fd7da59225
Shimada <my.sn.0308@gmail.com>
2022-03-11 14:56:25 +0900

    問い合わせ一覧画面のレイアウトを調整(admin.showInquiry.blade)

--- resources/views/admin/showInquiry.blade.php
+++ resources/views/admin/showInquiry.blade.php
@@ -1,21 +1,35 @@
 <x-header>
 {{-- $waiting $doneで出せるプルダウンで表示切替したい --}}
-<select name="answeredOrNot">
+@if(session('msg'))
+    {{ session('msg') }}<br>
+@endif
+<select name="answeredOrNot" id="select_box">
     <option value="1" selected>未回答</option>
     <option value="0">回答済み</option>
 </select>
 <div class="inquiryArea" style="padding: 20px;">
-    @foreach($waitings as $waiting)
-    <ul>
-        <li class="list-disc">
-            <a href="{{ route('inquiryDetail', $waiting['id']) }}">
-            ID：{{ $waiting['id'] }}
-            タイトル：{{ $waiting['title'] }}
-            </a><br>
-        </li>
-    @endforeach
+    <div class="waiting">
+        @foreach($waitings as $waiting)
+        <ul>
+            <li class="list-disc">
+                <a href="{{ route('inquiryDetail', $waiting['id']) }}">
+                ID：{{ $waiting['id'] }}
+                タイトル：{{ $waiting['title'] }}
+                </a><br>
+            </li>
+        @endforeach
+    </div>
+    <div class="done" style="display:none;">
+        @foreach($dones as $done)
+        <ul>
+            <li class="list-disc">
+                <a href="{{ route('inquiryDetail', $done['id']) }}">
+                ID：{{ $done['id'] }}
+                タイトル：{{ $done['title'] }}
+                </a><br>
+            </li>
+        @endforeach
+    </div>
 </div>
-{{-- {{dd($waiting);}} --}}
-
-
 </x-header>
+{{-- <script src="{{ mix('js/inquiryStatus.js') }}"></script> --}}
