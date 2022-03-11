4400a64a5a8de8ef4d9fc903b0c457864b682154
Shimada <my.sn.0308@gmail.com>
2022-03-11 14:43:15 +0900

    InquiryAnswerモデルに回答を 登録するメソッドを追加

--- app/Models/InquiryAnswer.php
+++ app/Models/InquiryAnswer.php
@@ -4,10 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class InquiryAnswer extends Model
 {
     use HasFactory;
 
+    protected $guarded = [
+        'id'
+    ];
+
     protected $table = 'inquiryAnswers';
+
+    public function createAnswer($request)
+    {
+        $data = [
+            'inquiry_id' => $request->inquiry_id,
+            'answerer_id' => Auth::id(),
+            'content' => $request->answer,
+        ];
+        $createdAnswer = $this->firstOrCreate($data);
+        
+        return $createdAnswer->id;
+    }
 }
