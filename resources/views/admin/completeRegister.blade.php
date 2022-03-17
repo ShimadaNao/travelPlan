d55b6502af14baff8b5cba9592bbb0ba7c469713
Shimada <my.sn.0308@gmail.com>
2022-03-17 13:35:21 +0900

    新規管理者登録完了画面のデザインを修正

--- resources/views/admin/completeRegister.blade.php
+++ resources/views/admin/completeRegister.blade.php
@@ -1,10 +1,16 @@
 <x-header>
     <div class="adminRegisterCompleted">
-    <p>{{ $registerMsg }}</p>
+    <h3 class="adminRegisterP">{{ $registerMsg }}</h3>
     @if($registeredData !== null)
-        <p>登録情報</p>
-        管理者：{{ $registeredData['name'] }}<br>
-        email：{{ $registeredData['email'] }}<br>
+        <p class="adminRegisteredArea">登録情報</p>
+        <ul class="mb-10">
+            <li>
+                <label class="inputLabel">管理者：</label>{{ $registeredData['name'] }}
+            </li>
+            <li>
+                <label class="inputLabel">email：</label>{{ $registeredData['email'] }}
+            </li>
+        </ul>
     @endif
     <a href="{{route('adminDashboard')}}">トップページへ</a>
     </div>
