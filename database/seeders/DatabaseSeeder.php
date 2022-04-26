f0a6e3f36157a0b21343a450402bf75d7bb0c2cb
Shimada <mysn03088@gmail.com>
2022-04-26 13:47:18 +0900

    DatabaseSeeder.phpをpostgres用に変更

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=0;'); mysql
+        DB::statement('SET CONSTRAINTS ALL DEFERRED;'); //postgres
 
         DB::table('users')->truncate();
         DB::table('plans')->truncate();
@@ -33,7 +34,8 @@ class DatabaseSeeder extends Seeder
             InquiryAnswerTableSeeder::class,
         ]);
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        DB::statement('SET CONSTRAINTS ALL IMMEDIATE;');
 
         // $this->call([
         //     AdminTableSeeder::class,
